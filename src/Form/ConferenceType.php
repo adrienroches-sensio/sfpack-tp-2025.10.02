4ff1bcb4cecac53e26503938e7647a0dffb70d9d
Adrien Roches <adrien@roc-it.tech>
2025-10-06 10:46:42 +0200

    [sf-start] Add auto generated form from conference entity

---
+++ src/Form/ConferenceType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Conference;
+use App\Entity\Organization;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConferenceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('accessible')
+            ->add('prerequisites')
+            ->add('startAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('endAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('organizations', EntityType::class, [
+                'class' => Organization::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Conference::class,
+        ]);
+    }
+}
