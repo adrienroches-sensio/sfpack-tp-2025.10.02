8aa1f9a3f011cb6ec92bd55d1b5add86ab116c19
Adrien Roches <adrien@roc-it.tech>
2025-10-06 14:11:09 +0200

    [sf-start] Create the User fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+
+final class UserFixtures extends Fixture
+{
+    private const USERS = [
+        [
+            'username' => 'admin',
+            'password' => 'admin',
+            'roles' => ['ROLE_ADMIN'],
+        ],
+    ];
+
+    public function __construct(
+        private readonly PasswordHasherFactoryInterface $passwordHasherFactory,
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (self::USERS as $userData) {
+            [
+                'username' => $username,
+                'password' => $password,
+                'roles' => $roles,
+            ] = $userData;
+
+            $user = (new User())
+                ->setUsername($username)
+                ->setPassword($this->passwordHasherFactory->getPasswordHasher(User::class)->hash($password))
+                ->setRoles($roles)
+            ;
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
