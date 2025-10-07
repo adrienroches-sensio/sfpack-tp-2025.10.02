53b19d82af9a9ac94e17a8c4292658a5bace34da
Adrien Roches <adrien@roc-it.tech>
2025-10-07 10:21:54 +0200

    [sf-advanced] Add DEVEVENTS_API_KEY env variable for conference api search

--- src/Search/DeveventsApiConferenceSearch.php
+++ src/Search/DeveventsApiConferenceSearch.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace App\Search;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
 final class DeveventsApiConferenceSearch implements ConferenceSearchInterface
 {
+    public function __construct(
+        #[Autowire(env: 'DEVEVENTS_API_KEY')]
+        private readonly string $apiKey,
+    ) {
+    }
+
     public function searchByName(string|null $name = null): array
     {
         // TODO: Implement searchByName() method.
