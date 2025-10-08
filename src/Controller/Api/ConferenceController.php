5016b30b6f3ed5074d1cd78719fcfb0d2bfe84eb
Adrien Roches <adrien@roc-it.tech>
2025-10-08 11:40:11 +0200

    [sf-advanced] Hndle circular references

--- src/Controller/Api/ConferenceController.php
+++ src/Controller/Api/ConferenceController.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 namespace App\Controller\Api;
 
+use App\Entity\Conference;
+use App\Entity\Organization;
+use App\Entity\User;
 use App\Repository\ConferenceRepository;
+use Doctrine\Common\Collections\Collection;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 final class ConferenceController
@@ -18,15 +23,44 @@ final class ConferenceController
     }
 
     #[Route(
-        path: '/api/conferences',
-        name: 'api_conferences_list',
+        path: '/api/conferences-callback',
+        name: 'api_conferences_list-fallback',
         methods: ['GET'],
     )]
-    public function list(): JsonResponse
+    public function listWithCallback(): JsonResponse
     {
         $conferences = $this->conferenceRepository->list();
 
-        $json = $this->serializer->serialize($conferences, 'json');
+        $json = $this->serializer->serialize($conferences, 'json', [
+            AbstractNormalizer::CALLBACKS => [
+                'organizations' => function (object $attributeValue, object $object, string $attributeName, ?string $format = null, array $context = []) {
+                    return $attributeValue->map(
+                        fn (Organization $organization) => $organization->getName()
+                    )->toArray();
+                },
+                'createdBy' => function (object $attributeValue, object $object, string $attributeName, ?string $format = null, array $context = []) {
+                    return $attributeValue->getUserIdentifier();
+                },
+            ],
+        ]);
+
+        return new JsonResponse($json, json: true);
+    }
+
+    #[Route(
+        path: '/api/conferences-circular',
+        name: 'api_conferences_list-circular',
+        methods: ['GET'],
+    )]
+    public function listWithCircular(): JsonResponse
+    {
+        $conferences = $this->conferenceRepository->list();
+
+        $json = $this->serializer->serialize($conferences, 'json', [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function (object $object, ?string $format, array $context): int {
+                return $object->getId();
+            },
+        ]);
 
         return new JsonResponse($json, json: true);
     }
